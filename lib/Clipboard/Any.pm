c321d06302bc9087cc38fce776b462af4bfc60ad
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-15 17:34:42 +0700

    Initial commit

---
+++ lib/Clipboard/Any.pm
@@ -0,0 +1,182 @@
+package Clipboard::Any;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Log::ger;
+
+use Exporter::Rinci qw(import);
+use File::Which qw(which);
+use IPC::System::Options 'system', 'readpipe', -log=>1;
+
+my $known_clipboard_managers = [qw/klipper/];
+my $sch_clipboard_manager = ['str', in=>$known_clipboard_managers];
+our %arg_clipboard_manager = (
+    clipboard_manager => {
+        summary => 'Explicitly set clipboard manager to use',
+        schema => $sch_screensaver,
+        description => <<'_',
+
+The default, when left undef, is to detect what clipboard manager is running,
+
+_
+    },
+);
+
+our %SPEC;
+
+$SPEC{':package'} = {
+    v => 1.1,
+    summary => 'Common interface to clipboard manager functions',
+    description => <<'_',
+
+This module provides common functions related to clipboard manager.
+
+Supported clipboard manager: KDE Plasma's Klipper (`klipper`). Support for more
+clipboard managers, e.g. on Windows or other Linux desktop environment is
+welcome.
+
+_
+};
+
+$SPEC{'detect_clipboard_manager'} = {
+    v => 1.1,
+    summary => 'Detect which clipboard manager program is currently running',
+    description => <<'_',
+
+Will return a string containing name of clipboard manager program, e.g.
+`klipper`. Will return undef if no known clipboard manager is detected.
+
+_
+    result_naked => 1,
+    result => {
+        schema => $sch_clipboard_manager,
+    },
+};
+sub detect_clipboard_manager {
+    my %args = @_;
+
+    #require Proc::Find;
+    #no warnings 'once';
+    #local $Proc::Find::CACHE = 1;
+
+  KLIPPER:
+    {
+        log_trace "Checking whether clipboard manager klipper is running ...";
+        unless (which "qdbus") {
+            log_trace "qdbus not found in PATH, system is probably not using klipper";
+            last;
+        }
+        my $out;
+        system({capture_merged=>\$out}, "qdbus", "org.kde.klipper", "/klipper");
+        unless ($? == 0) {
+            # note, when klipper is disabled via System Tray Settings > General
+            # > Extra Items, the object path /klipper disappears.
+            log_trace "Failed listing org.kde.klipper /klipper methods, system is probably not using klipper";
+            last;
+        }
+        log_trace "Concluding klipper is active";
+        return "klipper";
+    }
+
+    log_trace "No known clipboard manager is detected";
+    undef;
+}
+
+$SPEC{'clear_clipboard_history'} = {
+    v => 1.1,
+    summary => 'Delete all clipboard items',
+    description => <<'_',
+
+_
+    result => {
+        schema => $sch_clipboard_manager,
+    },
+};
+sub clear_clipboard_history {
+    my %args = @_;
+
+    my $clipboard_manager = $args{clipboard_manager} // detect_clipboard_manager();
+    return [412, "Can't detect any known clipboard manager"]
+        unless $clipboard_manager;
+
+    if ($clipboard_manager eq 'klipper') {
+        system "qdbus", "org.kde.klipper", "/klipper", "clearClipboardHistory";
+        my $exit_code = $? < 0 ? $? : $?>>8;
+        return [500, "/klipper's clearClipboardHistory failed: $exit_code"] if $exit_code;
+        return [200, "OK"];
+    }
+
+    [412, "Cannot clear clipboard history (clipboard manager=$clipboard_manager)"];
+}
+
+$SPEC{'get_clipboard_content'} = {
+    v => 1.1,
+    summary => 'Get the content of the current (most recent) item, a.k.a. history item [0]',
+    description => <<'_',
+
+Caveats for klipper: non-text content (e.g. image) is not retrievable by
+/klipper's getClipboardContents. If the current item is a non-text content, the
+most-recent item in the history is returned or empty string is returned if none
+exists.
+
+_
+    result => {
+        schema => $sch_clipboard_manager,
+    },
+};
+sub get_clipboard_content {
+    my %args = @_;
+
+    my $clipboard_manager = $args{clipboard_manager} // detect_clipboard_manager();
+    return [412, "Can't detect any known clipboard manager"]
+        unless $clipboard_manager;
+
+    if ($clipboard_manager eq 'klipper') {
+        my ($stdout, $stderr);
+        system({capture_stdout=>\$stdout, capture_stderr=>\$stderr},
+               "qdbus", "org.kde.klipper", "/klipper", "getClipboardContents");
+        my $exit_code = $? < 0 ? $? : $?>>8;
+        return [500, "/klipper's getClipboardContents failed: $exit_code"] if $exit_code;
+        return [200, "OK", $stdout];
+    }
+
+    [412, "Cannot clear clipboard history (clipboard manager=$clipboard_manager)"];
+}
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+This module provides a common interface to interact with clipboard.
+
+Some terminology:
+
+=over
+
+=item * clipboard content
+
+The current clipboard content. Some clipboard manager supports storing multiple
+items (multiple contents). All the items are called L</clipboard history>.
+
+=item * clipboard history
+
+Some clipboard manager supports storing multiple items (multiple contents). All
+the items are called clipboard history. It is presented as an array. The current
+item/content is at index 0, the secondmost current item is at index 1, and so
+on.
+
+=back
+
+
+=head1 NOTES
+
+On my system (KDE Plasma 5.12.9 on Linux), /klipper object's
+getClipboardHistoryItem() does not clear the current clipboard content. So I'm
+currently not providing a clear_clipboard_content function.
